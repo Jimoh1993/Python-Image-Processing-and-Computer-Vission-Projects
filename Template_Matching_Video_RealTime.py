2d0210bbba9b6e4fd77ccc3aafecbd953d8ee270
Jimoh Abdulganiyu <jimoh.abdulganiyu@aaua.edu.ng>
2019-10-24 14:16:23 +0100

    Add files via upload

---
+++ Template_Matching_Video_RealTime.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+cap = cv2.VideoCapture(0)
+template = cv2.imread("pen.jpg", cv2.IMREAD_GRAYSCALE)
+w, h = template.shape[::-1]
+while True:
+    _, frame = cap.read()
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    res = cv2.matchTemplate(gray_frame, template, cv2.TM_CCOEFF_NORMED)
+    loc = np.where(res >= 0.9)
+    for pt in zip(*loc[::-1]):
+        cv2.rectangle(frame, pt, (pt[0] + w, pt[1] + h), (0, 255, 0), 3)
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+cap.release()
+cv2.destroyAllWindows()
